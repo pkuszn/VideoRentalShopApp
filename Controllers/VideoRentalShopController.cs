b954409685e42135f551c853eaaecd9b5b75e1a1
patryk <patryk.kuszneruk1798@gmail.com>
2022-11-19 16:12:34 +0100

    controller conf - in progress

--- Controllers/VideoRentalShopController.cs
+++ Controllers/VideoRentalShopController.cs
@@ -1,6 +1,47 @@
-﻿namespace VideoRentalShopApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using VideoRentalShopApp.Interfaces;
+
+namespace VideoRentalShopApp.Controllers
 {
-    public class VideoRentalShopController
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoRentalShopController : ControllerBase
     {
+        private readonly ILogger<VideoRentalShopController> Logger;
+        private readonly IVideoRentalShopService VideoRentalShopService;
+
+        public VideoRentalShopController(ILogger<VideoRentalShopController> logger, IVideoRentalShopService videoRentalShopService)
+        {
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            VideoRentalShopService = videoRentalShopService ?? throw new ArgumentNullException(nameof(videoRentalShopService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
