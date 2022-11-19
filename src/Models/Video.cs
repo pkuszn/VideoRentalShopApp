62bb8d6aeb59c674ccf54de99fd9fb286dd4f641
patryk <patryk.kuszneruk1798@gmail.com>
2022-11-19 20:00:50 +0100

    added example data & modified video classes

--- src/Models/Video.cs
+++ src/Models/Video.cs
@@ -13,8 +13,8 @@ namespace VideoRentalShopApp.Models
         public string Title { get; set; }
         public string Genre { get; set; }
         public string Director { get; set; }
-        public DateTime Runtime { get; set; }
-        public int Score { get; set; }
+        public int Runtime { get; set; }
+        public double Score { get; set; }
         public string Description { get; set; }
         public List<string> Actors { get; set; }
         public DateTime CreatedDate { get; set; }
