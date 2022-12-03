7fa60a1bfae8e0542b80c905027a502a4dd0e4c8
patryk <patryk.kuszneruk1798@gmail.com>
2022-12-04 00:07:28 +0100

    get videos page

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿import { fetchVideos } from './fetch.js'
-import { createTableVideos } from './utils.js'
+import { createTableVideos, objectProperties, clearContent } from './utils.js'
 
 var container = document.getElementById('table-wrapper');
 var getVideosButton = document.getElementById('get-videos-button');
@@ -7,10 +7,19 @@ var getVideosButton = document.getElementById('get-videos-button');
 
 const getVideos = async () => {
     const response = await fetchVideos();
-    container = createTableVideos(response, container);
+    const headers = objectProperties(Object.values(response)[0]);
+    console.log(headers);
+    container = createTableVideos(response, container, headers);
 }
 
 getVideosButton.addEventListener('click', (e) => {
     e.preventDefault();
+    if(document.getElementsByClassName('fl-table').length > 0){
+        console.log("powinno czyscic");
+        clearContent(document.getElementsByClassName('fl-table'));
+        document.location.reload(); //TODO: do zrobienia
+    }
     getVideos();
 });
+
+
--- src/wwwroot/js/utils.js
+++ src/wwwroot/js/utils.js
@@ -1,31 +1,117 @@
-﻿function createTableVideos(videos, container) {
+﻿import { api } from './constants.js'
+// id, title, genre, director, runtime, score, description, actors, createdDate, isAvailable
+function createTableVideos(videos, container, headers) {
     if (videos === null) {
         return;
     }
-
+    console.log(videos);
     const table = document.createElement('table');
     const tableAttr = document.createAttribute('class');
     tableAttr.value = 'fl-table';
     table.setAttributeNode(tableAttr);
+
+    const tableHead = document.createElement('thead');
+    const trHead = document.createElement('tr');
+    for (let i = 0; i < headers.length; i++) {
+        const header = document.createElement('th');
+        const headerAttr = document.createAttribute('class');
+        headerAttr.value = 'headers';
+        header.setAttributeNode(headerAttr);
+        const headerText = document.createTextNode(headers[i]);
+        header.appendChild(headerText);
+        trHead.appendChild(header);
+    }
+    actionHeader(trHead);
+    tableHead.appendChild(trHead);
+
     const tableBody = document.createElement('tbody');
-
-
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < videos.length; i++) {
         const row = document.createElement('tr');
-
-        for (let j = 0; j < 2; j++) {
-            const cell = document.createElement('td');
-            const cellText = document.createTextNode(`cell in row${i}, column${j}`);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
-        }
+        row.appendChild(createRow(videos[i].id));
+        row.appendChild(createRow(videos[i].title));
+        row.appendChild(createRow(videos[i].genre));
+        row.appendChild(createRow(videos[i].director));
+        row.appendChild(createRow(videos[i].runtime));
+        row.appendChild(createRow(videos[i].score));
+        row.appendChild(createRow(videos[i].description));
+        row.appendChild(createRow(videos[i].actors));
+        row.appendChild(createRow(videos[i].createdDate));
+        row.appendChild(createRow(videos[i].isAvailable));
+        row.appendChild(actionRow());
         tableBody.appendChild(row);
     }
+    table.appendChild(tableHead);
     table.appendChild(tableBody);
     container.appendChild(table);
     return container;
 }
 
+const objectProperties = function (obj) {
+    let arr = [];
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            arr.push(prop);
+        }
+    }
+    return arr;
+};
+
+const createRow = function (element) {
+    const td = document.createElement('td');
+    const tdAttr = document.createAttribute('class');
+    tdAttr.value = 'cell';
+    td.setAttributeNode(tdAttr);
+    const tdText = document.createTextNode(element);
+    td.appendChild(tdText);
+    return td;
+}
+
+const actionRow = function(){
+    const div = document.createElement('div');
+    const divAttr = document.createAttribute('class');
+    divAttr.value = 'action-row';
+    div.setAttributeNode(divAttr);
+
+    const buttonDelete = document.createElement('a');
+    const buttonDeleteAttr = document.createAttribute('id');
+    const buttonDeleteAttrClass = document.createAttribute('class');
+    buttonDeleteAttrClass.value = 'button-row';
+    buttonDelete.setAttributeNode(buttonDeleteAttrClass);
+    buttonDeleteAttr.value = 'delete-row-button';
+    buttonDelete.setAttributeNode(buttonDeleteAttr);
+    buttonDelete.textContent = 'Delete';
+    
+    const buttonUpdate = document.createElement('a');
+    const buttonUpdateAttr = document.createAttribute("id");
+    const buttonUpdateAttrClass = document.createAttribute('class');
+    buttonUpdateAttrClass.value = 'button-row';
+    buttonUpdate.setAttributeNode(buttonUpdateAttrClass);
+    buttonUpdateAttr.value = 'update-row-button';
+    buttonUpdate.setAttributeNode(buttonUpdateAttr);
+    buttonUpdate.textContent = 'Update';
+
+    div.appendChild(buttonDelete);
+    div.appendChild(buttonUpdate);
+    return div;
+}
+
+
+const actionHeader = function(trHead){
+    const header = document.createElement('th');
+    const headerAttr = document.createAttribute('class');
+    headerAttr.value = 'headers';
+    header.setAttributeNode(headerAttr);
+    const headerText = document.createTextNode('action');
+    header.appendChild(headerText);
+    trHead.appendChild(header);
+}
+
+const clearContent = function(element){
+    element.innerHTML = "";
+}
+
 export {
-    createTableVideos
+    createTableVideos,
+    objectProperties,
+    clearContent
 }
