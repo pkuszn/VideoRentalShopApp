933cdf93f370d73faa4634b887216c64dc9763ac
pkuszneruk <pkuszneruk@10305-ARD>
2022-12-14 22:58:41 +0100

    added tests

---
+++ VideoRentaShopApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace VideoRentaShopApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
