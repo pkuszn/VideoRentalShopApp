75d1eb7bd4fb4a6ad3f3de25cfa2185ae5c7d010
pkuszneruk <pkuszneruk@10305-ARD>
2022-12-15 10:41:39 +0100

    assets

--- VideoRentaShopApp.Tests/Tests.cs
+++ VideoRentaShopApp.Tests/Tests.cs
@@ -1,6 +1,5 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
-using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,13 +9,6 @@ namespace VideoRentaShopApp.Tests
     public class Tests
     {
         [Fact]
-        public void Test1()
-        {
-            Console.WriteLine("test");
-        }
-
-
-        [Fact]
         public async Task GetVideosAsync_NotEmpty_True()
         {
             using WebApplicationFactory<VideoRentalShopApp.Startup> app = new WebApplicationFactory<VideoRentalShopApp.Startup>();
@@ -24,6 +16,17 @@ namespace VideoRentaShopApp.Tests
 
             System.Net.Http.HttpResponseMessage response = await client.GetAsync("/VideoRentalShop/GetVideos");
             
+            response.Content.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetUsersAsync_NotEmpty_True()
+        {
+            using WebApplicationFactory<VideoRentalShopApp.Startup> app = new WebApplicationFactory<VideoRentalShopApp.Startup>();
+            using System.Net.Http.HttpClient client = app.CreateClient();
+
+            System.Net.Http.HttpResponseMessage response = await client.GetAsync("/VideoRentalShop/GetUsers");
+
             response.Content.Should().NotBeNull();
         }
     }
