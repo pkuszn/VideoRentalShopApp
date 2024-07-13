228a86aff778562d35052834eaddd8c4705da379
patryk <patryk.kuszneruk1798@gmail.com>
2024-07-13 17:53:35 +0200

    manual approach

--- run_docker.sh
+++ run_docker.sh
@@ -8,10 +8,10 @@ echo $vrsa
 
 echo "> Creating an image..."
 if sudo docker image inspect $vrsa >/dev/null 2>&1; then
-	echo "Image exists locally"
+    echo "Image exists locally"
 else
-	echo "Image does not exists locally"
-	sudo docker build -t $vrsa -f Dockerfile .
+    echo "Image does not exist locally"
+    sudo docker build -t $vrsa -f Dockerfile .
 fi
 
 echo "> Creating a $vrsa container..."
@@ -23,14 +23,15 @@ if [ ! "$(sudo docker ps -a -q -f name=$vrsa)" ]; then
 fi
 
 echo "> Pulling a mongodb image..."
-sudo docker pull $mongo:latest
+sudo docker pull mongo:latest
 
-echo "> Creating a mongodb container..."
-if [ ! "$(sudo docker ps -a -q -f name=$mongo)" ]; then
-    if [ "$(sudo docker ps -aq -f status=exited -f name=$mongo)" ]; then
-        sudo docker rm -f $mongo
+echo "> Creating a mongodb container with initialization..."
+if [ ! "$(sudo docker ps -a -q -f name=mongodb)" ]; then
+    if [ "$(sudo docker ps -aq -f status=exited -f name=mongodb)" ]; then
+        sudo docker rm -f mongodb
     fi
-    sudo docker run -d -p 27017:27017 --name mongodb $mongo
+    sudo docker build -t mongo-init -f Dockerfile.mongo-init .
+    sudo docker run -d -p 27017:27017 --name mongodb mongo-init
 fi
 
 echo "> Starting a $vrsa container..."
@@ -39,6 +40,6 @@ if [ "$(sudo docker ps -aq -f status=exited -f name=$vrsa)" ]; then
 fi
 
 echo "> Starting a $mongo container..."
-if [ "$(sudo docker ps -aq -f status=exited -f name=$mongo)" ]; then
-    sudo docker start $mongo
+if [ "$(sudo docker ps -aq -f status=exited -f name=mongodb)" ]; then
+    sudo docker start mongodb
 fi
